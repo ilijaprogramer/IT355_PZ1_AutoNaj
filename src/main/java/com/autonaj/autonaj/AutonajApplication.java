c3316ecdad4feb9673b8be41db008744fda3f5dc
Ilija Trajkovic <ilijatrajkovic64@gmail.com>
2026-07-15 15:50:35 +0200

    Inicijalni Spring Boot skelet i pocetna strana

---
+++ src/main/java/com/autonaj/autonaj/AutonajApplication.java
@@ -0,0 +1,13 @@
+package com.autonaj.autonaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutonajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutonajApplication.class, args);
+	}
+
+}
